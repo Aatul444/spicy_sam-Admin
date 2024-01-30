2f952c0dac7e9b9026806bb638d270422e429a0c
Atul Chaudhary <a.atulchaudhary444@gmail.com>
2024-01-30 09:19:58 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'spicy_sam-Admin',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
